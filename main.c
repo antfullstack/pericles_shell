fd8531491a413cd3e3c6e9cf02a6a41dc49f1cb9
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-03-11 20:10:15 +0200

    Improved base architecture to facilitate the use of more complexity.

--- main.c
+++ main.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "path_track.h"
 
 int main(int argc, char *argv[]) {
   while (1) {
-    setbuf(stdout, NULL);
-    printf("A: ");
+    short found_command = 0;
 
-    char command[1024];
-    fgets(command, sizeof(command), stdin);
-    command[strcspn(command, "\n")] = '\0';
-
-    if (strcmp(command, "exit") == 0) {
+    if (avalidate(argv[1], "exit", argc, 2) == 0) {
+      found_command = 1;
+      printf("Terminating...");
       break;
-      
-    } else if (strncmp(command,"stream ",7) == 0){
-      printf("%s\n", command + 7);
+    }
 
-    } else if (strncmp(command,"typeof ",7) == 0){
-      char *buffer = command + 7;
+    if (avalidate(argv[1], "ostream", argc, 3) == 0) {
+      found_command = 1;
+      printf("%s", argv[2]);
+    }
 
-      if ((buffer != NULL) == 0 || strcmp(buffer, "exit") == 0 || strcmp(buffer, "stream") == 0 ) {
-        printf("%s is a shell builtin\n", buffer);
-      } else if (findexec(buffer) == 0){
-        printf("%s is an outside executable file\n", buffer);
+    if(avalidate(argv[1], "typeof", argc, 3) == 0) {
+      found_command = 1;
+      if (strcmp(argv[2], "exit") == 0 || strcmp(argv[2], "ostream") == 0 || strcmp(argv[2], "typeof") == 0) { //NEEDS HASH TABLE SEARCHING (WHICH NEEDS HASH TABLE IMPLEMENTATION)
+        printf("%s is a shell builtin.", argv[2]);
+      } else if (findexec(argv[2]) == 0){
+        printf("%s is an executable file in PATH.", argv[2]);
       } else {
-        printf("%s is not a shell builtin nor a recognised executable in PATH\n", buffer);
+        printf("%s doesn't seem to be a shell builtin or an executable in PATH.");
       }
+    }
 
-    } else if (strncmp(command,"launch ", 7) == 0){
-      char *buffer = command + 7;
-      if (findexec(buffer) == 0) {
-        system(buffer);
-      } else {
-        printf("%s: command not found\n",command);
-      }
-    } else if (strcmp(command, "clear") == 0) {
-      system("clear");
-    } else {
-      if (findexec(command) == 0) {
-        system(command);
-      } else {
-        printf("%s: command not found\n",command);
+    if(isbuiltin(argv[1]) != 0 && findexec(argv[1]) == 0) {
+      int id = fork();
+      if (id == 0) {
+        execvp(argv[1], argv);
       }
     }
   }
   return 0;
+}
+
+int avalidate(char* argument, char* command, int argc, int expected_argc) {
+  if (strcmp(argument, command) && argc == expected_argc) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+int isbuiltin(char* name) {
+  if (strcmp(name, "exit") == 0 || strcmp(name, "ostream") == 0 || strcmp(name, "typeof") == 0) {
+    return 0;
+  }
+
+  return -1;
 }
