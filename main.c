95cad98ab311d766aff87a550c1fc75f69d02525
antfullstack <253273528+antfullstack@users.noreply.github.com>
2026-03-28 19:57:41 +0200

    Added cd command

--- main.c
+++ main.c
@@ -13,7 +13,9 @@ int main() {
   while (1) {
     fflush(stdout);
 
-    printf("A: ");
+    char dir_buf[200];
+    getcwd(dir_buf, sizeof(dir_buf));
+    printf("%s A: ", dir_buf);
     char command[1056]; 
     int command_capacity = sizeof(command);
     fgets(command, command_capacity, stdin);
@@ -26,37 +28,33 @@ int main() {
         short found_command = 0;
 
         if (avalidate(argv[1], "exit", argc, 2) == 0) {
-        found_command = 1;
-        printf("Terminating...\n");
-        break;
+            found_command = 1;
+            printf("Terminating...\n");
+            break;
+        }
+
+        if (avalidate(argv[1], "cd", argc, 3) == 0) {
+            found_command = 1; 
+
+            int dir_status = chdir(argv[2]);
+            if (dir_status == -1) {
+                printf("%s is not a valid directory.\n", argv[2]);
+            } 
         }
 
         if (strcmp(argv[1], "ostream")== 0 && argc > 2) {
-        found_command = 1;
-        for (int i = 2; i < argc; i++) {
-            printf("%s ", argv[i]);
-        }
-        printf("\n");
+            found_command = 1;
+            for (int i = 2; i < argc; i++) {
+                printf("%s ", argv[i]);
+            }
+            printf("\n");
         }
 
         if (argc > 2 && (avalidate(argv[1], "sneak", argc, 3) == 0 || avalidate(argv[1], "sneak", argc, 4) == 0)) {
-        found_command = 1;
+            found_command = 1;
 
-        if (argc == 3) {
-            char* result = scanall(argv[2]);
-            if (result == NULL) {
-            printf("Could not fetch file.");
-            } else {        
-            printf("%s\n", result);
-            free(result);
-            }
-        } else {
-            if (atoi(argv[3]) <= 0) {
-            printf("Invalid number of lines provided.");
-            } else {
-            int argc_final = atoi(argv[3]);
-            if (argc_final > 1) {
-                char* result = sneak(argv[2], argc_final);
+            if (argc == 3) {
+                char* result = scanall(argv[2]);
                 if (result == NULL) {
                 printf("Could not fetch file.");
                 } else {        
@@ -64,52 +62,65 @@ int main() {
                 free(result);
                 }
             } else {
-                printf("Number of lines must be at least 1.");
+                if (atoi(argv[3]) <= 0) {
+                    printf("Invalid number of lines provided.");
+                } else {
+                int argc_final = atoi(argv[3]);
+                if (argc_final > 1) {
+                    char* result = sneak(argv[2], argc_final);
+                    if (result == NULL) {
+                    printf("Could not fetch file.");
+                    } else {        
+                    printf("%s\n", result);
+                    free(result);
+                    }
+                } else {
+                    printf("Number of lines must be at least 1.");
+                }
+                }
             }
-            }
-        }
         }
 
         if (avalidate(argv[1], "scanall", argc, 3) == 0) {
-        found_command = 1;
-        char* result = scanall(argv[2]);
-        if (result == NULL) {
-            printf("Could not fetch file.");
-        } else {        
-            printf("%s\n", result);
-            free(result);
-        }
-        }
+            found_command = 1;
+            char* result = scanall(argv[2]);
+            if (result == NULL) {
+                printf("Could not fetch file.");
+            } else {        
+                printf("%s\n", result);
+                free(result);
+            }
+            }
 
         if (avalidate(argv[1], "fenv", argc, 3) == 0 && argv) {
-        found_command = 1;
-        if (argv[2] != NULL && getenv(argv[2]) != NULL) {
-            char* env = getenv(argv[2]);
-            printf("Value of %s is %s\n", argv[2], env);
-        }
+            found_command = 1;
+            if (argv[2] != NULL && getenv(argv[2]) != NULL) {
+                char* env = getenv(argv[2]);
+                printf("Value of %s is %s\n", argv[2], env);
+            }
         }
 
         if(avalidate(argv[1], "typeof", argc, 3) == 0) {
-        found_command = 1;
-        if (isbuiltin(argv[2]) == 0) {
-            printf("%s is a shell builtin.\n", argv[2]);
-        } else if (findexec(argv[2]) == 0){
-            printf("%s is an executable file in PATH.\n", argv[2]);
-        } else {
-            printf("%s doesn't seem to be a shell builtin or an executable in PATH.\n", argv[2]);
-        }
+            found_command = 1;
+            if (isbuiltin(argv[2]) == 0) {
+                printf("%s is a shell builtin.\n", argv[2]);
+            } else if (findexec(argv[2]) == 0){
+                printf("%s is an executable file in PATH.\n", argv[2]);
+            } else {
+                printf("%s doesn't seem to be a shell builtin or an executable in PATH.\n", argv[2]);
+            }
         }
 
         if(argc > 1 && isbuiltin(argv[1]) != 0 && findexec(argv[1]) == 0) {
-        found_command = 1;
-        int exec_code = execvp(argv[1], argv + 1);
-        if (exec_code == -1) {
-            printf("Could not execute\n");
-        } 
+            found_command = 1;
+            int exec_code = execvp(argv[1], argv + 1);
+            if (exec_code == -1) {
+                printf("Could not execute\n");
+            } 
         }
 
         if (found_command == 0) {
-        printf("Command not found\n");
+            printf("Command not found\n");
         }
 
     } else {
